1dae768bd2115dd10f74079b292dbff2905a9157
Brandon <brandon@Brandons-Mac-mini.local>
2025-10-29 10:09:04 +0700

    (feat): Implemented skeleton loader

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+/* --------------------Skeleton Loader-------------------------*/
+
+// Get all elements in the document (for skeleton loader)
+const contentElements = document.querySelectorAll("*");
+
+// Add skeleton classes when DOM is ready
+document.addEventListener("DOMContentLoaded", () => {
+  contentElements.forEach((el) => {
+    el.classList.add("skeleton");
+  });
+});
+
+// Remove skeleton classes when everything has loaded
+window.onload = () => {
+  contentElements.forEach((el) => {
+    el.classList.remove("skeleton");
+  });
+};
+
+/* -------------------------------------------------------------*/
+
+
+
